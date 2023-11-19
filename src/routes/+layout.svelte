11eb8f97da27def9b8e665eb14add42492b0e878
ZackarySantana <zackzackyack@gmail.com>
2023-11-18 19:49:53 -0500

    Add theme system

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,30 @@
 	import '../app.postcss';
 </script>
 
-<div>
-	<header>
-		<h1>My app</h1>
+<div class="light-fire max-w-[500px] mx-auto">
+	<header class="flex justify-center items-center py-4">
+		<a
+			href="https://github.com/ZackarySantana/Attach2"
+			target="_blank"
+			class="text-attach-accent font-mono text-sm"
+		>
+			|attach|
+		</a>
 	</header>
 
-	<main>
+	<main class="px-4">
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	<footer class="px-5 sm:px-10 flex justify-center items-center py-8">
+		<a href="https://github.com/ZackarySantana/Attach2" target="_blank">
+			<p class="text-sm">
+				Putting the power of connecting in to the hands of the user. <br />
+				Theme: <code>Light Fire</code>
+			</p>
+			<h3>
+				Click here to make your own <span class="text-attach-accent font-mono">|attach|</span>
+			</h3>
+		</a>
 	</footer>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,68 @@
+const plugin = require('tailwindcss/plugin');
+
+const themes = ['light-fire'];
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'attach-accent': '#4ea8ed',
+				'light-fire': {
+					'text-primary': '#000000',
+					'text-secondary': '#ffffff',
+					'text-chip': '#ffffff',
+					bg: '#ffffff',
+					'bg-primary': '#f3f4f6',
+					'bg-secondary': '#d04952',
+					'bg-chip': '#d04952',
+					accent: '#f3f4f6'
+				}
+			}
+		}
 	},
 
-	plugins: []
+	plugins: [
+		plugin(function ({ addUtilities, theme }) {
+			themes.forEach((t) => {
+				addUtilities({
+					[`.${t} .text-primary`]: {
+						color: theme(`colors.${t}.text-primary`)
+					},
+					[`.${t} .text-secondary`]: {
+						color: theme(`colors.${t}.text-secondary`)
+					},
+					[`.${t} .text-chip`]: {
+						color: theme(`colors.${t}.text-chip`)
+					},
+					[`.${t} .fade-line`]: {
+						maskImage: `linear-gradient(90deg, currentColor 75%, transparent 90%)`
+					},
+					[`body:has(.${t})`]: {
+						backgroundColor: theme(`colors.${t}.bg`),
+						color: theme(`colors.${t}.text-primary`)
+					},
+					[`.${t} .bg`]: {
+						backgroundColor: theme(`colors.${t}.bg`)
+					},
+					[`.${t} .bg-primary`]: {
+						backgroundColor: theme(`colors.${t}.bg-primary`)
+					},
+					[`.${t} .bg-secondary`]: {
+						backgroundColor: theme(`colors.${t}.bg-secondary`)
+					},
+					[`.${t} .bg-chip`]: {
+						backgroundColor: theme(`colors.${t}.bg-chip`)
+					},
+					[`.${t} .accent`]: {
+						fill: theme(`colors.${t}.accent`)
+					}
+				});
+			});
+		})
+	]
 };
 
 module.exports = config;
