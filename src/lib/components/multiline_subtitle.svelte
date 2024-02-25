d981af079e2f1ddacf1c279dddcfff37523d912f
ZackarySantana <zackzackyack@gmail.com>
2024-02-24 19:50:14 -0500

    refactor: ordering

--- src/lib/components/multiline_subtitle.svelte
+++ src/lib/components/multiline_subtitle.svelte
@@ -1,4 +1,4 @@
-<div class="text-primary-subtitle relative pl-3 text-sm leading-4">
+<div class="relative pl-3 text-sm leading-4 text-primary-subtitle">
     <div
         class="absolute left-[2px] top-[11%] h-[80%] w-[2px] rounded-sm bg-accent"
     />
